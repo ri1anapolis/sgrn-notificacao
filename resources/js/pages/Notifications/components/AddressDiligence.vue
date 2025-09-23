506ff9bcfbcdfc495d60b03b028b931ade7345c7
gustaribeiror <gustarsoftwr@gmail.com>
2025-09-22 22:12:33 -0300

    updating

---
+++ resources/js/pages/Notifications/components/AddressDiligence.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+const props = defineProps({
+    completeAddress: {
+        type: String,
+        required: true,
+    }
+})
+
+</script>
+
+<template>
+    <div class="-mt-3">
+        <div class="flex bg-[#0e1423] border-2 border-[#b3925c21] h-auto w-auto max-w-4xl pt-5 p-5 m-auto text-white rounded-lg">
+            <div class="flex flex-col w-full">
+                <strong class="text-bege-claro font- text-xl mb-5">Endereço da Notificação:</strong>
+                <div class="w-full group flex items-center">
+                    <span class="text-lg">
+                        {{ props.completeAddress }}
+                    </span>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
